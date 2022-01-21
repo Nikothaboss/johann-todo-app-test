f0941dfd86f1b43f5fb201e45ea6f6560322cbc1
Nikothaboss <57292271+Nikothaboss@users.noreply.github.com>
2022-01-21 13:11:21 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import Todo from './components/Todo';
+import { GlobalStyle } from './App.styles';
+import { useEffect, useState } from 'react/cjs/react.development';
+import { size } from './App.styles';
 
 function App() {
+  // !test
+
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  const [numberOfChar, setNumberOfChar] = useState(20);
+
+  const resize = () => {
+    setScreenWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    setNumberOfChar(() => {
+      if (screenWidth > size.mobileXL) {
+        return 60;
+      } else if (screenWidth > size.mobileL) {
+        return 40;
+      }
+    });
+    window.addEventListener('resize', resize);
+    return () => {
+      window.removeEventListener('resize', resize);
+    };
+  }, [resize, numberOfChar]);
+  // !test
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <Todo numberOfChar={numberOfChar} />
+    </>
   );
 }
 
---
+++ src/components/Todo.js
@@ -0,0 +1,100 @@
+import React, { useEffect, useState } from 'react';
+import { StyledDiv } from './Todo.styled';
+import List from './List';
+import Alert from './Alert';
+
+const Todo = ({ numberOfChar }) => {
+  const [inputValue, setInputValue] = useState('');
+  const [list, setList] = useState(() => {
+    return localStorage.getItem('list')
+      ? JSON.parse(localStorage.getItem('list'))
+      : [];
+  });
+  const [editFlag, setEditFlag] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({ show: false, type: '', msg: '' });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!inputValue) {
+      showAlert(true, 'danger', 'Please enter Value');
+    } else if (editFlag && inputValue) {
+      setList(
+        list.map((item) => {
+          if (item.id === editID) {
+            return { ...item, inputValue };
+          }
+          return item;
+        })
+      );
+      showAlert(true, 'success', 'Item edited');
+    } else {
+      const newItem = {
+        id: new Date().getTime().toString(),
+        inputValue,
+      };
+      setList((prev) => {
+        return [...prev, newItem];
+      });
+      showAlert(true, 'success', 'item added');
+    }
+  };
+
+  const showAlert = (show = false, type = '', msg = '') => {
+    setAlert({ show, type, msg });
+  };
+
+  const clearList = () => {
+    setList([]);
+    showAlert(true, 'danger', 'List cleared');
+  };
+
+  const deleteItem = (id) => {
+    setList(list.filter((item) => item.id !== id));
+    showAlert(true, 'danger', 'Item removed');
+  };
+
+  const editItem = (id) => {
+    const editItem = list.filter((item) => item.id === id);
+    setEditID(id);
+    setInputValue(editItem[0].inputValue);
+    setEditFlag(true);
+  };
+
+  useEffect(() => {
+    setEditFlag(false);
+    setInputValue('');
+    localStorage.setItem('list', JSON.stringify(list));
+  }, [list]);
+
+  return (
+    <StyledDiv>
+      <section className='wrapper'>
+        <Alert {...alert} showAlert={showAlert} list={list} />
+        <h2>Todo App</h2>
+        <form action='' onSubmit={handleSubmit}>
+          <input
+            type='text'
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+          <button type='submit' className='submit-btn'>
+            {editFlag ? 'Edit' : 'Submit'}
+          </button>
+        </form>
+        {list.length > 0 && (
+          <List
+            list={list}
+            clearList={clearList}
+            deleteItem={deleteItem}
+            editItem={editItem}
+            numberOfChar={numberOfChar}
+          />
+        )}
+      </section>
+    </StyledDiv>
+  );
+};
+
+export default Todo;
